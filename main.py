f47624122a234da95ac4871bc5d654481be975f1
tarequzzaman-nexttech <t.khan@nextech.com>
2026-06-22 15:31:19 +0930

    Australian holidays api added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from fastapi import FastAPI, HTTPException, Query
 
-app = FastAPI(title="Australia Holidays API")
+app = FastAPI(title="Australian Holidays API")
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
